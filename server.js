a405037d212a8711abff6129c642e3399a53c935
Codefident <piotr.klep01@wp.pl>
2021-06-15 03:42:48 +0200

    zaznaczanie, start gry

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const path = require('path')
+//const path = require('path')
 const io = require('socket.io')(server, { cors: { origin: '*' } })
 const port = 3000
 
 let players = []
+let gameRunning = false
 
 app.use(express.static('static'))
 
@@ -20,13 +21,13 @@ io.on('connection', socket => {
     socket.on('login', nickname => {
         if (players.length < 2) {
             if (players.length == 1) {
-                players.push({ nickname: nickname, id: socket.id })
+                players.push({ nickname: nickname, id: socket.id, ready: false, warships: [], points: 0 })
                 socket.emit('login', { dodano: true, id: socket.id })
                 socket.emit('oponent', players[0].nickname)
                 socket.broadcast.emit('oponent', nickname)
             }
             else {
-                players.push({ nickname: nickname, id: socket.id })
+                players.push({ nickname: nickname, id: socket.id, ready: false, warships: [], points: 0 })
                 socket.emit('login', { dodano: true, id: socket.id })
             }
         }
@@ -45,6 +46,22 @@ io.on('connection', socket => {
             }
         }
         console.log(players)
+    })
+
+    socket.on('ready', data => {
+        for (i in players) {
+            if (players[i].id == data.id) {
+                players[i].ready = true
+                players[i].warships = data.warships
+                console.log('set up ' + players[i].nickname + ' status to ready')
+            }
+        }
+        try {
+            if (players[0].ready && players[1].ready) {
+                io.emit('start')
+            }
+        }
+        catch (err) { }
     })
 
 })
--- static/js/Square.js
+++ static/js/Square.js
@@ -10,16 +10,25 @@ class Square {
     append() {
         let seaSingleSquare = document.createElement('div')
         seaSingleSquare.classList.add('seasquare')
-        seaSingleSquare.onclick = () => this.click()
+        seaSingleSquare.onclick = () => this.clickAndSetUpWarship()
         this.element = seaSingleSquare
         document.getElementById('seaContainer').appendChild(seaSingleSquare)
     }
 
-    click() {
+    clickAndSetUpWarship() {
         this.boat = !this.boat
-        if (this.boat)
+        if (this.boat) {
             this.element.style.background = 'blue'
-        else
+            game.warshipsLocations.push({ 'x': this.x, 'y': this.y })
+        }
+        else {
             this.element.style.background = 'none'
+            for (let i in game.warshipsLocations) {
+                if (game.warshipsLocations[i].x == this.x && game.warshipsLocations[i].y == this.y) {
+                    game.warshipsLocations.splice(i, 1)
+                }
+            }
+        }
+        console.log(game.warshipsLocations)
     }
 }
